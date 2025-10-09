ccd0871daa1cd9ea1d3caa868d5b485fbd809aa4
Faznug_Space <153061199+FaishalFaiz@users.noreply.github.com>
2025-10-09 08:44:19 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./tailwind.css";
+
+const API_KEY = "AIzaSyDzZCGfS8aQ4tlM4TK1efVmh7M9FXKJUGg";
+
+export default function App() {
+
+  return (
+    <>
+      <nav className="w-full p-4 bg-white border-b border-gray-200 flex items-center justify-between">
+        <h1>Cari Aja</h1>
+      </nav>
+    </>
+  );
+}
